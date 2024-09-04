662d327b1f597d01c53987eb7ff950b87a07ae3a
Devanshu <devgoyal952@gmail.com>
2024-09-04 21:52:28 +0530

    created Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,28 @@
 import React from "react";
 import "./Navbar.css";
-import logo from "../../assets/logo2png.png";
+import logo from "../../assets/logo-no-background.png";
+import arrowIcon from "../../assets/arrow_icon.png";
 
 const Navbar = () => {
   return (
     <div className="navbar">
-      <img src={logo} alt="logo" />
+      <img src={logo} alt="logo" className="logo" />
+      <ul>
+        <li>Home</li>
+        <li>Features</li>
+        <li>Pricing</li>
+        <li>Blog</li>
+      </ul>
+      <div className="nav-right">
+        <select>
+          <option value="usd">USD</option>
+          <option value="euro">EURO</option>
+          <option value="inr">INR</option>
+        </select>
+        <button>
+          Sign up <img src={arrowIcon} alt="arrow-icon" />
+        </button>
+      </div>
     </div>
   );
 };
