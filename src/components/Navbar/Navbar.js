ac9b61084bb01d60b242e6e5e95c892afe7dfda3
Devanshu <devgoyal952@gmail.com>
2024-09-04 18:31:35 +0530

    imported logo in navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./Navbar.css";
+import logo from "../../assets/logo2png.png";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <img src={logo} alt="logo" />
+    </div>
+  );
 };
 
 export default Navbar;
