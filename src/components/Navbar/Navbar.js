cdda8852555aea3c66c80c2be9dd1b8af44febea
Devanshu <devgoyal952@gmail.com>
2024-09-07 10:59:14 +0530

    Added currency in navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Navbar.css";
 import logo from "../../assets/logo-no-background.png";
 import arrowIcon from "../../assets/arrow_icon.png";
+import { CoinContext } from "../../context/CoinContext";
 
 const Navbar = () => {
+  const { setCurrency } = useContext(CoinContext);
+  const currencyHandler = (event) => {
+    switch (event.target.value) {
+      case "usd": {
+        setCurrency({ name: "usd", symbol: "$" });
+        break;
+      }
+      case "euro": {
+        setCurrency({ name: "eur", symbol: "€" });
+        break;
+      }
+      case "inr": {
+        setCurrency({ name: "inr", symbol: "₹" });
+        break;
+      }
+      default: {
+        setCurrency({ name: "usd", symbol: "$" });
+        break;
+      }
+    }
+  };
+
   return (
     <div className="navbar">
       <img src={logo} alt="logo" className="logo" />
@@ -14,7 +37,7 @@ const Navbar = () => {
         <li>Blog</li>
       </ul>
       <div className="nav-right">
-        <select>
+        <select onChange={currencyHandler}>
           <option value="usd">USD</option>
           <option value="euro">EURO</option>
           <option value="inr">INR</option>
