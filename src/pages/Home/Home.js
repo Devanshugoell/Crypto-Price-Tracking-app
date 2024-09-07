87b33e60548a3917a81151253ddcdc0c13b442ef
Devanshu <devgoyal952@gmail.com>
2024-09-07 10:57:53 +0530

    Displayed coin

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./Home.css";
+import { CoinContext } from "../../context/CoinContext";
 
 const Home = () => {
+  const { allCoin, currency } = useContext(CoinContext);
+  const [displayCoin, setDisplayCoin] = useState([]);
+
+  useEffect(() => {
+    setDisplayCoin(allCoin);
+  }, [allCoin]);
+
   return (
     <div className="home">
       <div className="hero">
@@ -25,6 +33,30 @@ const Home = () => {
           <p>24H Change</p>
           <p>Market Cap</p>
         </div>
+        {displayCoin.map((item, index) => {
+          return (
+            <div className="table-layout" key={index}>
+              <p>{item.market_cap_rank}</p>
+              <div className="coin">
+                <img src={item.image} alt="Crypto-image" />
+                <p>{item.name.slice(0, 15) + " - " + item.symbol}</p>
+              </div>
+              <p>
+                {currency.symbol} {item.current_price.toLocaleString()}
+              </p>
+              <p
+                className={
+                  item.price_change_percentage_24h > 0 ? "green" : "red"
+                }
+              >
+                {Math.floor(item.price_change_percentage_24h * 100) / 100}
+              </p>
+              <p>
+                {currency.symbol} {item.market_cap.toLocaleString()}
+              </p>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
