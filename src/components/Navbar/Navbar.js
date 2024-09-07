5753319f270363e6e7d7ee7293765df5ec927f95
Devanshu <devgoyal952@gmail.com>
2024-09-07 18:24:38 +0530

    imported Link from react-router-dom

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,6 +3,7 @@ import "./Navbar.css";
 import logo from "../../assets/logo-no-background.png";
 import arrowIcon from "../../assets/arrow_icon.png";
 import { CoinContext } from "../../context/CoinContext";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const { setCurrency } = useContext(CoinContext);
@@ -29,9 +30,13 @@ const Navbar = () => {
 
   return (
     <div className="navbar">
-      <img src={logo} alt="logo" className="logo" />
+      <Link to={"/"}>
+        <img src={logo} alt="logo" className="logo" />
+      </Link>
       <ul>
-        <li>Home</li>
+        <Link to={"/"}>
+          <li>Home</li>
+        </Link>
         <li>Features</li>
         <li>Pricing</li>
         <li>Blog</li>
